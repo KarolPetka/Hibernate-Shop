6da9fa522257f1811888cbf6ded2ecb6bd97faab
KarolPetka <karolpetka1@gmail.com>
2020-08-26 16:31:18 +0200

    Implemented save() and findById() and created successfully running test for these methods for Employee entity, delete() and findAll() test still set to fail()

--- src/main/java/project/database/daoImpl/EmployeeDaoImpl.java
+++ src/main/java/project/database/daoImpl/EmployeeDaoImpl.java
@@ -1,17 +1,52 @@
 package project.database.daoImpl;
 
+import org.hibernate.Session;
 import project.database.dao.EmployeeDao;
 import project.database.entity.Employee;
+import project.database.utils.HibernateUtils;
 
+import javax.persistence.NoResultException;
 import java.util.List;
 
 public class EmployeeDaoImpl implements EmployeeDao {
-    public void save(Employee employee) {
 
+    public void save(Employee employee) {
+        Session session = HibernateUtils
+                .instance()
+                .getSessionFactory()
+                .getCurrentSession();
+
+        session.beginTransaction();
+
+        session.saveOrUpdate(employee);
+
+        session.getTransaction().commit();
+        session.close();
     }
 
     public Employee findById(Long id) {
-        return null;
+        Session session = HibernateUtils
+                .instance()
+                .getSessionFactory()
+                .getCurrentSession();
+
+        session.beginTransaction();
+
+        Employee employee = null;
+
+        try {
+            employee = (Employee) session
+                    .createQuery("from Employee where id=:id")
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            e.printStackTrace();
+        }
+
+        session.getTransaction().commit();
+        session.close();
+
+        return employee;
     }
 
     public List<Employee> findAll() {
--- src/test/java/project/database/daoImpl/EmployeeDaoImplTest.java
+++ src/test/java/project/database/daoImpl/EmployeeDaoImplTest.java
@@ -1,19 +1,58 @@
 package project.database.daoImpl;
 
+import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import project.database.dao.EmployeeDao;
+import project.database.entity.Employee;
+import project.database.utils.HibernateUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeDaoImplTest {
 
-    @Test
-    void save() {
-        fail();
+    private EmployeeDao employeeDao = new EmployeeDaoImpl();
+
+    @BeforeEach
+    void setUp(){
+        try {
+            Session session = HibernateUtils
+                    .instance()
+                    .getSessionFactory()
+                    .getCurrentSession();
+
+            session.beginTransaction();
+
+            session.createQuery("delete Employee")
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+            session.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+            fail(e);
+        }
     }
 
     @Test
-    void findById() {
-        fail();
+    void saveAndFindById() {
+        try {
+            Employee employee = new Employee();
+            employee.setName("John Smith");
+
+            employeeDao.save(employee);
+
+            Employee loadedEmployee = employeeDao.findById(employee.getEmployeeId());
+
+            assertNotNull(loadedEmployee);
+            assertEquals(employee.getEmployeeId(), loadedEmployee.getEmployeeId());
+            assertEquals(employee.getName(), loadedEmployee.getName());
+
+        } catch (Exception e){
+            e.printStackTrace();
+            fail(e);
+        }
     }
 
     @Test
