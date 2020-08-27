d622188bb561cc86c0b768b90c9b00a82f85fc26
KarolPetka <karolpetka1@gmail.com>
2020-08-27 14:48:00 +0200

    Created save() and findById() method and successfully running test case for Location entity

--- src/main/java/project/database/daoImpl/LocationDaoImpl.java
+++ src/main/java/project/database/daoImpl/LocationDaoImpl.java
@@ -1,17 +1,51 @@
 package project.database.daoImpl;
 
+import org.hibernate.Session;
 import project.database.dao.LocationDao;
 import project.database.entity.Location;
+import project.database.utils.HibernateUtils;
 
 import java.util.List;
 
 public class LocationDaoImpl implements LocationDao {
-    public void save(Location location) {
 
+    public void save(Location location) {
+        Session session = HibernateUtils
+                .instance()
+                .getSessionFactory()
+                .getCurrentSession();
+
+        session.beginTransaction();
+
+        session.saveOrUpdate(location);
+
+        session.getTransaction().commit();
+        session.close();
     }
 
     public Location findById(Long id) {
-        return null;
+        Session session = HibernateUtils
+                .instance()
+                .getSessionFactory()
+                .getCurrentSession();
+
+        session.beginTransaction();
+
+        Location location = null;
+
+        try {
+            location = (Location) session.createQuery("from Location where id=:id")
+                    .setParameter("id", id)
+                    .getSingleResult();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        session.getTransaction().commit();
+        session.close();
+
+        return location;
     }
 
     public List<Location> findAll() {
--- src/test/java/project/database/daoImpl/LocationDaoImplTest.java
+++ src/test/java/project/database/daoImpl/LocationDaoImplTest.java
@@ -1,14 +1,55 @@
 package project.database.daoImpl;
 
+import org.hibernate.Session;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import project.database.dao.EmployeeDao;
+import project.database.dao.LocationDao;
+import project.database.entity.Employee;
+import project.database.entity.Location;
+import project.database.utils.HibernateUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LocationDaoImplTest {
 
+    private LocationDao locationDao = new LocationDaoImpl();
+
+    @BeforeEach
+    void setUp() {
+        try {
+            Session session = HibernateUtils
+                    .instance()
+                    .getSessionFactory()
+                    .getCurrentSession();
+
+            session.beginTransaction();
+
+            session.createQuery("delete Employee")
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+            session.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e);
+        }
+    }
+
     @Test
-    void save() {
-        fail();
+    void saveAndFindById() {
+        try {
+            Location location = new Location();
+            location.setCountry("USA");
+            location.setCity("Los Angeles");
+
+            locationDao.save(location);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            fail(e);
+        }
     }
 
     @Test
